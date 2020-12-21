a217eb73c018e3714b4078bdf4c7dc3f5abfe582
Danila <Gdanila2002@yandex.ru>
2020-12-21 22:38:43 +0300

    задание 2

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ keysStr=keysStr.slice(0,-2);
 console.log(keysStr);
 outputStr=`Студент ${student.first_name} ${student.last_name} учится в группе ${student.group}.`;
 console.log(outputStr);
+
+popArray=['к', 'e', 'к', 2, 2, 8];
+function popper(popArray) {
+    length=popArray.length;
+    for(let i=0; i<length; i++){
+        let popStr=popArray.pop();
+        if(popArray.length>0){
+            popStr+=" осталось ["+popArray+']';
+        }
+        console.log(popStr);
+    }
+}
+popper(popArray);
